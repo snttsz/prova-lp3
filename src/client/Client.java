b265ac50065c8a4faee6e20d749fd2157638bc59
Acso Notebook <glendaeea1@gmail.com>
2024-05-05 12:07:07 -0300

    removidos comentatios do client

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,16 +10,12 @@ public class Client {
     {
         try {
 
-            String host = "25.35.154.184"; // Usar o endereço IP correto do servidor RMI
+            String host = "25.35.154.184";
             int port = 1099;
 
-            // Obtém o registro RMI no servidor usando o endereço IP e porta especificados
             Registry registry = LocateRegistry.getRegistry(host, port);
-
-            // Procura o objeto remoto registrado com o nome "ListaDeTarefas" no registro RMI
             InterfaceRMI stub = (InterfaceRMI) registry.lookup("ListaDeTarefas");
 
-            // Chama o método remoto para criar uma nova lista de tarefas
             System.out.println("Criando a lista de tarefas: " + stub.criarListaDeTarefas("LP3"));
             System.out.println("Nome da tarefa: " + stub.getListaDeTarefas("LP3").getNomeDaLista());
 
